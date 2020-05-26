8156cb2bbd3837d10afacf96e7193f3fedd7a6d7
Diego Torres <diegotc86@gmail.com>
2020-05-26 07:27:20 -0500

    Add index, show, new and create action methods and views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def game_params
+    params.require(:game).permit(:name, :summary, :release_date, :category, :rating, :parent_id)
+  end
+end
