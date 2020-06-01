3361fc3c3b3210893095b42ab2fe35b1eec78b6b
Diego Torres <diegotc86@gmail.com>
2020-05-31 21:36:29 -0500

    Add create and destroy genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,31 @@ class GenresController < ApplicationController
   def index
     @genres = Genre.all.order(:name)
   end
+
+  def new
+    @game = Game.find(params[:game_id])
+    @genre = @game.genres.new
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @genre = Genre.find_or_initialize_by(genre_params)
+    if @genre.save
+      @game.genres << @genre
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:game_id])
+    @genre = Genre.find(params[:id])
+    @game.genres.delete(@genre)
+    redirect_to game_path(@game)
+  end
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "games#index"
-  resources :games
+  resources :games do
+    resources :genres, only: [:new, :create, :destroy]
+  end
   resources :companies, only: :index
   resources :genres, only: :index
   resources :platforms, only: :index
