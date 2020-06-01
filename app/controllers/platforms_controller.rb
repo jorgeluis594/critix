72e321f3bacdb603400edb413e08391d9650195b
Diego Torres <diegotc86@gmail.com>
2020-05-31 22:35:15 -0500

    Add create and destroy platforms

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -2,4 +2,31 @@ class PlatformsController < ApplicationController
   def index
     @platforms = Platform.all.order(:name)
   end
+
+  def new
+    @game = Game.find(params[:game_id])
+    @platform = @game.platforms.new
+  end
+
+  def create
+    @game = Game.find(params[:game_id])
+    @platform = Platform.find_or_initialize_by(platform_params)
+    if @platform.save
+      @game.platforms << @platform
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:game_id])
+    @platform = Platform.find(params[:id])
+    @game.platforms.delete(@platform)
+    redirect_to game_path(@game)
+  end
+
+  def platform_params
+    params.require(:platform).permit(:name, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "games#index"
   resources :games do
     resources :genres, only: [:new, :create, :destroy]
+    resources :platforms, only: [:new, :create, :destroy]
   end
   resources :companies, only: :index
   resources :genres, only: :index
